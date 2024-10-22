7c821cfb67fa0dc799fc7c98612f64e4e89d0f0c
Anton Rikhnovets <rihnovec93@mail.ru>
2024-10-22 21:17:30 +0900

    Настраивает валидацию полей фильтра по цене

--- src/components/FilterForm.vue
+++ src/components/FilterForm.vue
@@ -1,7 +1,7 @@
 <script setup>
-import {ref, computed, onMounted, watch} from 'vue'
-import {storeToRefs} from 'pinia'
-import {useCatalogStore} from '@/stores/catalog-store/index.js'
+import { ref, computed, onMounted, watch } from 'vue'
+import { storeToRefs } from 'pinia'
+import { useCatalogStore } from '@/stores/catalog-store/index.js'
 
 import {
   NGrid,
@@ -13,12 +13,13 @@ import {
   NInput,
   NButton,
   NSpace,
-  NEl
+  NEl,
 } from 'naive-ui'
 
 const catalogStore = useCatalogStore()
-const {categories, selectedCategoryValue, priceRange} = storeToRefs(catalogStore)
-const {fetchCategories, resetFilter, applyFilter} = catalogStore
+const { categories, selectedCategoryValue, priceRange } =
+  storeToRefs(catalogStore)
+const { fetchCategories, resetFilter, applyFilter } = catalogStore
 
 const minPriceValue = ref(priceRange.value.min)
 const maxPriceValue = ref(priceRange.value.max)
@@ -32,16 +33,16 @@ const options = computed(() => {
   const categoriesOptions = categories.value.map(category => {
     return {
       label: category.slice(0, 1).toUpperCase() + category.slice(1),
-      value: category
+      value: category,
     }
   })
 
   return [
     {
       label: 'Все категории',
-      value: 'all'
+      value: 'all',
     },
-    ...categoriesOptions
+    ...categoriesOptions,
   ]
 })
 const hasOptions = computed(() => categories.value.length > 0)
@@ -62,6 +63,15 @@ function onInputMax(value) {
   maxPriceValue.value = value
 }
 
+function onChangeFilterPrice() {
+  minPriceValue.value = Math.max(priceRange.value.min, minPriceValue.value)
+
+  maxPriceValue.value = Math.max(
+    minPriceValue.value,
+    Math.min(priceRange.value.max, maxPriceValue.value),
+  )
+}
+
 onMounted(() => {
   fetchCategories()
 })
@@ -70,13 +80,28 @@ onMounted(() => {
 <template>
   <n-layout>
     <n-layout-content content-style="padding: 24px;">
-      <n-card title="Фильтр" :bordered="false" content-style="padding: 14px 0;" header-style="padding: 0">
+      <n-card
+        title="Фильтр"
+        :bordered="false"
+        content-style="padding: 14px 0;"
+        header-style="padding: 0"
+      >
         <n-space vertical>
           <n-el class="control-title">Выберите категорию</n-el>
-          <n-select :value="selectedCategoryValue" :disabled="!hasOptions" :options="options" @update-value="changeCategory" />
+          <n-select
+            :value="selectedCategoryValue"
+            :disabled="!hasOptions"
+            :options="options"
+            @update-value="changeCategory"
+          />
         </n-space>
       </n-card>
-      <n-card title="Цена" :bordered="false" content-style="padding: 14px 0;" header-style="padding: 0">
+      <n-card
+        title="Цена"
+        :bordered="false"
+        content-style="padding: 14px 0;"
+        header-style="padding: 0"
+      >
         <n-space vertical>
           <n-grid x-gap="14" :cols="2">
             <n-gi>
@@ -87,6 +112,7 @@ onMounted(() => {
                   :allow-input="allowInputCheck"
                   placeholder="Мин. цена"
                   inputmode="number"
+                  :on-change="onChangeFilterPrice"
                   :on-update:value="onInputMin"
                 />
               </n-space>
@@ -99,6 +125,7 @@ onMounted(() => {
                   :allow-input="allowInputCheck"
                   placeholder="Макс. цена"
                   inputmode="number"
+                  :on-change="onChangeFilterPrice"
                   :on-update:value="onInputMax"
                 />
               </n-space>
@@ -121,7 +148,7 @@ onMounted(() => {
 </template>
 
 <style lang="scss" scoped>
-  .control-title {
-    font-weight: 500;
-  }
+.control-title {
+  font-weight: 500;
+}
 </style>
