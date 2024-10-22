8fec9d5bed1655ff4d72b799e91d3ce544cde492
Anton Rikhnovets <rihnovec93@mail.ru>
2024-10-22 23:20:37 +0900

    Прогресс добавления TS

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,32 @@
-<script setup>
-import {
-  NLayout
-} from 'naive-ui'
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import type { Ref } from 'vue'
+import { NLayout } from 'naive-ui'
+// import VHeader from '@/components/VHeader.vue'
+// import VBody from '@/components/VBody.vue'
+// import { useRouter } from 'vue-router'
 
-import VHeader from '@/components/VHeader.vue'
-import VBody from '@/components/VBody.vue'
-import {useRouter} from 'vue-router'
+export default defineComponent({
+  name: 'App',
+  components: {
+    NLayout,
+  },
+  setup() {
+    const appName: Ref<string> = ref('Shop Mini App')
 
-const router = useRouter()
+    return { appName }
+  },
+})
+
+// const router = useRouter()
 </script>
 
 <template>
   <n-layout>
-    <VHeader v-if="router.currentRoute.value.name !== 'auth'" />
-    <VBody />
+    Test TS APP {{ appName }}
+    <!-- <VHeader v-if="router.currentRoute.value.name !== 'auth'" /> -->
+    <!-- <VBody /> -->
   </n-layout>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { createApp } from 'vue'
-import { store } from '@/stores/index.js'
+import { store } from './stores/index.ts'
 
 import App from './App.vue'
-import router from './router'
+// import router from './router/index'
 
 const app = createApp(App)
 
 app.use(store)
-app.use(router)
+// app.use(router)
 
 app.mount('#app')
